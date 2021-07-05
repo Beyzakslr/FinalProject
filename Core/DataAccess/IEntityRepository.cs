96d9f36b693274605852e059494e35d8b5988c9b
Beyza Kesler <beyzakslr@hotmail.com>
2021-07-05 15:52:59 +0300

    05.07.21

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,12 @@
-﻿using Entities.Abstract;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
+
 {
     //generic constraint => where T:class
     //Class: referans tip olabilir
